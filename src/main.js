de1b950ab6d510d45ecb1918c53eb410398fceae
amir <amirtaki623@gmail.com>
2025-08-19 20:28:04 +0330

    part 15

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ download.addEventListener("click", (e) => {
     download.classList.add('start');
     let degree = 0;
     let interval = setInterval(() => {
-        
+        degree ++;
+        if(degree > 100){
+            clearInterval(interval)
+            number.textContent = `<i class="fa-solid fa-check"></i>`
+        }
     })
 })
