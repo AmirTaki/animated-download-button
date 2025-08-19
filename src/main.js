87ea80a9dd4e6dee892918e20b32c2b5f93149f8
amir <amirtaki623@gmail.com>
2025-08-19 20:25:35 +0330

    part 14

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 const download = document.querySelector('.download');
+const container = document.querySelector("#container");
+const number = document.querySelector("#number")
 download.addEventListener("click", (e) => {
     download.classList.add('start');
+    let degree = 0;
+    let interval = setInterval(() => {
+        
+    })
 })
